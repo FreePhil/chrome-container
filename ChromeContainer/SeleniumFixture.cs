e71aa17a80554b2c0d229c36b98fd6ad9e37d2e4
Phil <phil@hengetech.com.tw>
2023-06-16 17:31:09 +0800

    change test site

---
+++ ChromeContainer/SeleniumFixture.cs
@@ -0,0 +1,70 @@
+﻿using Docker.DotNet.Models;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Networks;
+
+namespace ChromeContainer;
+
+public class SeleniumFixture: IAsyncLifetime
+{
+    public INetwork Network { get; set; }
+    public IContainer Hub { get; set; }
+    public IContainer Browser { get; set; }
+    public IContainer Recorder { get; set; }
+    
+    public async Task InitializeAsync()
+    {
+        Network = new NetworkBuilder()
+            .WithName("seleniumnetwork")
+            .Build();
+        Hub = new ContainerBuilder()
+            .WithImage("selenium/hub:latest")
+            .WithName("selenium_hub")
+            .WithPortBinding(4442, 4442)
+            .WithPortBinding(4443, 4443)
+            .WithPortBinding(4444, 4444)
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(4444))
+            .WithNetwork(Network)
+            .Build();
+
+        var env = new Dictionary<string, string>
+        {
+            { "SE_EVENT_BUS_HOST", "selenium_hub" },
+            { "SE_EVENT_BUS_PUBLISH_PORT", "4442" },
+            { "SE_EVENT_BUS_SUBSCRIBE_PORT", "4443" }
+        };
+
+        Browser = new ContainerBuilder()
+            .WithImage("selenium/node-chrome:latest")
+            .WithName("selenium_chrome")
+            .WithEnvironment(env)
+            .WithNetwork(Network)
+            .Build();
+        
+        var recorderEnv = new Dictionary<string, string>
+        {
+            { "DISPLAY_CONTAINER_NAME", "selenium_chrome" },
+            { "FILE_NAME", "chrome.mp4" }
+        };
+        Recorder = new ContainerBuilder()
+            .WithImage("selenium/video:latest")
+            .WithName("selenium_video")
+            .WithEnvironment(recorderEnv)
+            .WithNetwork(Network)
+            .Build();
+            
+
+        await Network.CreateAsync();
+        await Hub.StartAsync();
+        await Browser.StartAsync();
+        await Recorder.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Recorder.StopAsync();
+        await Browser.StopAsync();
+        await Hub.StopAsync();
+        await Network.DeleteAsync();
+    }
+}
---
+++ ChromeContainer/SimpleSeleniumWithTestContainerTest.cs
@@ -0,0 +1,71 @@
+using SeleniumExtras.WaitHelpers;
+
+namespace ChromeContainer;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using Xunit;
+
+public class SimpleSeleniumWithTestContainerTest: IClassFixture<SeleniumFixture>
+{
+    private SeleniumFixture fixture;
+    
+    public SimpleSeleniumWithTestContainerTest(SeleniumFixture fixture)
+    {
+        this.fixture = fixture;
+    }
+    
+    [Theory]
+    [InlineData("standard_user", "secret_sauce")]
+    [InlineData("problem_user", "secret_sauce")]
+    [InlineData("performance_glitch_user", "secret_sauce")]
+    public void TestLoginSuccess(string username, string password)
+    {
+        var remote = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"),  new ChromeOptions());
+        remote.Navigate().GoToUrl("https://www.saucedemo.com/");
+        remote.Manage().Window.Maximize();
+        var inputUsername = remote.FindElementById("user-name");
+        var inputPassword = remote.FindElementById("password");
+
+        var buttonLogin = remote.FindElementById("login-button");
+
+        inputUsername.SendKeys(username);
+        inputPassword.SendKeys(password);
+
+        buttonLogin.Click();
+        
+        var waiter = new WebDriverWait(remote, TimeSpan.FromSeconds(1));
+        By nextPageElementSelector = By.Id("add-to-cart-sauce-labs-backpack");
+
+        waiter.Until(ExpectedConditions.ElementIsVisible(nextPageElementSelector));
+        // remote.Quit();
+    }
+    
+    [Theory]
+    [InlineData("standard_user", "secret_sauce1")]
+    [InlineData("abc", "secret_sauce")]
+    public void TestLoginFailure(string username, string password)
+    {
+        var remote = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"),  new ChromeOptions());
+        remote.Navigate().GoToUrl("https://www.saucedemo.com/");
+        remote.Manage().Window.Maximize();
+        var inputUsername = remote.FindElementById("user-name");
+        var inputPassword = remote.FindElementById("password");
+
+        var buttonLogin = remote.FindElementById("login-button");
+
+        inputUsername.SendKeys(username);
+        inputPassword.SendKeys(password);
+        
+        buttonLogin.Click();
+        
+        var waiter = new WebDriverWait(remote, TimeSpan.FromSeconds(1));
+        By errorMessageElement = By.CssSelector("div.error-message-container.error");
+
+        waiter.Until(ExpectedConditions.ElementIsVisible(errorMessageElement));
+
+        // remote.Quit();
+    }
+}
